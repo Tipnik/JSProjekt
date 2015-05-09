f0f6af49ddbeb0d9393523ade4b920ca263574e2
Tipnik <mkmichalkozlowski@gmail.com>
2015-05-10 01:25:14 +0200

    PlanszaJSON
    Plansza z JSON, kilka mniejszych zmian

--- game.js
+++ game.js
@@ -4,20 +4,60 @@ function preload() {
 	var str = [1,2,3,4,5];
 	str = str.splice(0, 1);
 	console.log(str);
-	game.load.image('sky', 'assets/sky.png');
-    game.load.image('ground', 'assets/platform.png');
-    game.load.image('star', 'assets/star.png');
+
+	game.load.tilemap('map', 'assets/map.json', null, Phaser.Tilemap.TILED_JSON);
+
+    game.load.image('ground_1x1', 'assets/ground_1x1.png');
+    game.load.image('walls_1x2', 'assets/walls_1x2.png');
+    game.load.image('tiles2', 'assets/tiles2.png');
+
+    game.load.spritesheet('coin', 'assets/coin.png', 32, 32);
+
     game.load.spritesheet('dude', 'assets/dude.png', 32, 48);
 
 }
 
+
+var cursors;
+var map;
+var coins;
+
+var layer;
+var layer2;
+var sprite;
+
 function create() {
+
+	map = game.add.tilemap('map');
+
+    map.addTilesetImage('ground_1x1');
+    map.addTilesetImage('walls_1x2');
+    map.addTilesetImage('tiles2');
+
+    map.setCollisionBetween(1, 12);
+
+    layer = map.createLayer('Tile Layer 1');
+
+    layer2 = map.createLayer('Tile Layer 2');
+
+    layer.resizeWorld();
 
 	//  We're going to be using physics, so enable the Arcade Physics system
     game.physics.startSystem(Phaser.Physics.ARCADE);
 
     //  A simple background for our game
-    game.add.sprite(0, 0, 'sky');
+    //game.add.sprite(0, 0, 'sky');
+
+    //  Here we create our coins group
+    coins = game.add.group();
+    coins.enableBody = true;
+
+    //  And now we convert all of the Tiled objects with an ID of 34 into sprites within the coins group
+    map.createFromObjects('Object Layer 1', 34, 'coin', 0, true, false, coins);
+
+    //  Add animations to all of the coin sprites
+    coins.callAll('animations.add', 'animations', 'spin', [0, 1, 2, 3, 4, 5], 10, true);
+    coins.callAll('animations.play', 'animations', 'spin');
 
     //  The platforms group contains the ground and the 2 ledges we can jump on
     platforms = game.add.group();
@@ -26,45 +66,58 @@ function create() {
     platforms.enableBody = true;
 
     // Here we create the ground.
-    var ground = platforms.create(0, game.world.height - 64, 'ground');
+    //var ground = platforms.create(0, game.world.height - 64, 'ground');
 
     //  Scale it to fit the width of the game (the original sprite is 400x32 in size)
-    ground.scale.setTo(2, 2);
+    //ground.scale.setTo(2, 2);
 
     //  This stops it from falling away when you jump on it
-    ground.body.immovable = true;
+    //ground.body.immovable = true;
 
     //  Now let's create two ledges
-    var ledge = platforms.create(400, 400, 'ground');
+    //var ledge = platforms.create(400, 400, 'ground');
 
-    ledge.body.immovable = true;
+    //ledge.body.immovable = true;
 
-    ledge = platforms.create(-150, 300, 'ground');
+    //ledge = platforms.create(-150, 300, 'ground');
 
-    ledge.body.immovable = true;
+    //ledge.body.immovable = true;
 
-        // The player and its settings
+    // The player and its settings
     player = game.add.sprite(32, game.world.height - 150, 'dude');
+
 
     //  We need to enable physics on the player
     game.physics.arcade.enable(player);
 
-    //  Player physics properties. Give the little guy a slight bounce.
+    // Resize world
+    //game.world.resize(6000, 600);
+
+    // Camera follow the player
+    game.camera.follow(player, Phaser.Camera.FOLLOW_PLATFORMER);
+
+    // Player physics properties. Give the little guy a slight bounce.
     player.body.bounce.y = 0.2;
     player.body.gravity.y = 450;
     player.body.collideWorldBounds = true;
+    player.body.setSize(25, 44, 3, 4);
 
-    //  Our two animations, walking left and right.
+    // Our two animations, walking left and right.
     player.animations.add('left', [0, 1, 2, 3], 10, true);
     player.animations.add('right', [5, 6, 7, 8], 10, true);
 
     cursors = game.input.keyboard.createCursorKeys();
 
+    jumpButton = game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
+
 }
 
 function update() {
 
-	game.physics.arcade.collide(player, platforms);
+	//game.physics.arcade.collide(player, platforms);
+	game.physics.arcade.collide(player, layer);
+	game.physics.arcade.overlap(player, coins, collectCoin, null, this);
+	game.debug.body(player);
 
 	 //  Reset the players velocity (movement)
     player.body.velocity.x = 0;
@@ -92,9 +145,18 @@ function update() {
     }
 
     //  Allow the player to jump if they are touching the ground.
-    if (cursors.up.isDown && player.body.touching.down)
+    
+	if ((cursors.up.isDown || jumpButton.isDown) && player.body.onFloor())
+    // if ((cursors.up.isDown || jumpButton.isDown) && player.body.touching.down)
     {
         player.body.velocity.y = -350;
     }
+
+
+function collectCoin(player, coin) {
+
+    coin.kill();
+
+}
 
 }
